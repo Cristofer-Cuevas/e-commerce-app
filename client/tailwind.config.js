4a6c4ad9f67971a493a76eb1360b3fecf6dda32d
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-09 22:33:08 -0400

    icon svg added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "user-icon": "url('/src/styles/images/person_black_24dp.svg')",
+        "lock-icon": "url('/src/styles/images/lock_black_24dp.svg')",
+        "mail-icon": "url('/src/styles/images/email_black_24dp.svg')",
+      },
+    },
   },
   plugins: [],
 };
