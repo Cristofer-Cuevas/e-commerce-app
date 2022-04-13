d4d05c14a27f5125628a1bba54756cf9365ca5a8
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-12 21:46:27 -0400

    add show and hide menu

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,14 +8,27 @@ import closeIcon from "../styles/images/close_white_24dp.svg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const handleMenuClick = (e) => {};
-  const handleCloseClick = (e) => {};
-
+  const navBarRef = useRef(null);
   const inputRef = useRef(null);
+  // Show menu
+  const handleMenuClick = (e) => {
+    navBarRef.current.classList.remove("-left-full");
+    setTimeout(() => {
+      navBarRef.current.classList.add("left-0", "w-64");
+    }, 1);
+  };
+  // Hide menu
+  const handleCloseClick = (e) => {
+    navBarRef.current.classList.remove("w-64");
+    setTimeout(() => {
+      navBarRef.current.classList.add("-left-full");
+    }, 1);
+  };
+
   return (
     <>
       <section className="bg-hero-image bg-cover bg-center   h-5/6">
-        <header className="w-11/12 mx-auto">
+        <header className="w-11/12 mx-auto overflow">
           <h1 className="font-bold text-4xl text-center text-white pt-8 ">Shop</h1>
           <div className="flex justify-between mt-10">
             <p className="w-20 text-white">
@@ -34,12 +47,12 @@ const Home = () => {
               <span className="bg-search-icon bg-no-repeat bg-center bg-orange-600 inline-block w-10 h-10 rounded-r"> </span>
             </div>
           </div>
-          <nav className="fixed bg-black h-full top-0 left-0 text-white w-0 z-50">
-            <div className="flex h-16 justify-end pr-5">
+          <nav className="fixed overflow-hidden bg-black h-full top-0 -left-full text-white z-50" ref={navBarRef}>
+            <div className="flex h-16 justify-end pr-5  top-2.5">
               <img className="w-8" onClick={handleCloseClick} src={closeIcon} alt="Close" />
             </div>
             <ul className="">
-              <li className=" pl-4 py-4 text-2xl">
+              <li className="pl-4 py-4 text-2xl">
                 <Link to="/">Home</Link>
               </li>
               <li className="pl-4 text-2xl">
