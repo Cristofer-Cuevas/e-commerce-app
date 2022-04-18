55a9485d9340edab695d44bacbdd62dd364b824a
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-17 22:39:35 -0400

    Remove Loading component import

--- client/src/components/Fashion.js
+++ client/src/components/Fashion.js
@@ -1,39 +1,20 @@
 import React, { useEffect, useState, useRef } from "react";
-import { ProductBox } from "./ComponentUtils";
+import { Products, Loading } from "./ComponentUtils";
 const Fashion = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(false);
 
   useEffect(() => {
-    (async () => {
-      await fetch("https://fakestoreapi.com/products/category/women's clothing")
-        .then((res) => res.json())
-        .then((res) => {
-          setData((data) => {
-            return [...res, ...data];
-          });
-          console.log(res);
-        });
-    })();
-    (async () => {
-      await fetch("https://fakestoreapi.com/products/category/men's clothing")
-        .then((res) => res.json())
-        .then((res) => {
-          setData((data) => {
-            return [...res, ...data];
-          });
-          console.log(res);
-        });
-    })();
+    fetch("http://localhost:3001/clothes")
+      .then((res) => res.json())
+      .then((res) => setData(res.clothes));
   }, []);
   return (
-    <section className="flex flex-col items-center">
-      <h1 className="font-bold text-3xl text-center">Man and Woman Fashion</h1>
-      {data
-        ? data.map((clothe) => {
-            return <ProductBox key={clothe.id} product={clothe} />;
-          })
-        : null}
-    </section>
+    <>
+      <section className="w-full pb-20">
+        <h1 className="font-bold text-3xl text-center">Man and Woman Fashion</h1>
+        <Products products={data} />
+      </section>
+    </>
   );
 };
 
