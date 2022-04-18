282cdefc8765f92e010db085ae7f0c932f128947
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-17 22:42:06 -0400

    v1

---
+++ client/src/components/Electronic.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { Products } from "./ComponentUtils";
+const Electronic = () => {
+  const [data, setData] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/electronics")
+      .then((res) => res.json())
+      .then((res) => setData(res.electronics));
+  }, []);
+  return (
+    <>
+      <section className="w-full pb-20">
+        <h1 className="font-bold text-3xl text-center">Electronic</h1>
+        <Products products={data} />
+      </section>
+    </>
+  );
+};
+
+export default Electronic;
