f1422219d87f34317c67ff1d58b0e771bc47ebb4
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-20 22:45:39 -0400

    add imports of componenst

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Electronic from "./components/Electronic";
+import Jewellery from "./components/Jewellery";
+import Fashion from "./components/Fashion";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home Fashion={Fashion} Jewellery={Jewellery} Electronic={Electronic} />} />
+        <Route path="/jewellery" element={<Home Jewellery={Jewellery} />}></Route>
+        <Route path="/Fashion" element={<Home Fashion={Fashion} />} />
+        <Route path="/electronic" element={<Home Electronic={Electronic} />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
