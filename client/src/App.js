325442ca59147c0fa5b10ccb3adab90ec8673a5e
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-21 21:54:41 -0400

    passing componenst as children of Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useCallback, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
@@ -9,18 +9,58 @@ import Fashion from "./components/Fashion";
 import Cart from "./components/Cart";
 
 function App() {
+  const [productsCount, setProductsCount] = useState(null);
+  const [productsInCart, setProductsInCart] = useState([]);
+
+  useEffect(() => {
+    setProductsCount(productsInCart.length);
+  }, [productsInCart]);
+
+  console.log(productsCount);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home Fashion={Fashion} Jewellery={Jewellery} Electronic={Electronic} />} />
-        <Route path="/jewellery" element={<Home Jewellery={Jewellery} />}></Route>
-        <Route path="/Fashion" element={<Home Fashion={Fashion} />} />
-        <Route path="/electronic" element={<Home Electronic={Electronic} />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home>
+                <Fashion setProductsInCart={setProductsInCart} />
+                <Jewellery setProductsInCart={setProductsInCart} />
+                <Electronic setProductsInCart={setProductsInCart} />
+              </Home>
+            }
+          />
+          <Route
+            path="/jewellery"
+            element={
+              <Home>
+                <Jewellery setProductsInCart={setProductsInCart} />
+              </Home>
+            }
+          />
+          <Route
+            path="/Fashion"
+            element={
+              <Home>
+                <Fashion setProductsInCart={setProductsInCart} />
+              </Home>
+            }
+          />
+          <Route
+            path="/electronic"
+            element={
+              <Home>
+                <Electronic setProductsInCart={setProductsInCart} />
+              </Home>
+            }
+          />
+          <Route path="/cart" element={<Cart productsInCart={productsInCart} />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
