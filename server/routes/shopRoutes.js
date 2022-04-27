31ee198b2193adfaccb77be1761da298961b2046
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-26 22:31:37 -0400

    add /jewellery and /electronics routes

--- server/routes/shopRoutes.js
+++ server/routes/shopRoutes.js
@@ -5,4 +5,8 @@ const shopRoutes = express.Router();
 
 shopRoutes.get("/clothes", shopControllers.getClothes);
 
+shopRoutes.get("/jewellery", shopControllers.getJewellery);
+
+shopRoutes.get("/electronics", shopControllers.getElectronics);
+
 export default shopRoutes;
