39b65c2095472dd11fe75933865e613e896a7385
programmerXS <cristoferperezmejia80@gmail.com>
2022-04-30 15:08:12 -0400

    Creating the user credit object everytime a new user signs up

--- server/controllers/userAuthControllers.js
+++ server/controllers/userAuthControllers.js
@@ -12,8 +12,6 @@ const postAuthRequest = async (url, body, res) => {
 
   const { success, token, user, userExists, isPasswordValid, anInternalErrorOccurred } = await response.json();
 
-  // console.log(success, token, userExists, isPasswordValid, anInternalErrorOccurred);
-
   if (success) {
     const { rows: credit } = await pool.query("SELECT credit FROM credits WHERE user_id = $1", [user.id]);
     user.credit = credit[0].credit;
@@ -42,7 +40,7 @@ userAuthControllers.signin = async (req, res) => {
   postAuthRequest(url, body, res);
 };
 
-userAuthControllers.signup = (req, res) => {
+userAuthControllers.signup = async (req, res) => {
   const { name, lastName, email, password } = req.body;
   const url = "https://user-auth-restful-api.herokuapp.com/email-signup";
   const body = JSON.stringify({
@@ -52,7 +50,25 @@ userAuthControllers.signup = (req, res) => {
     password,
   });
 
-  postAuthRequest(url, body, res);
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body,
+  });
+
+  const { success, token, user, userExists, anInternalErrorOccurred } = await response.json();
+
+  if (success) {
+    console.log(user);
+    const { rows: credit } = await pool.query("INSERT INTO credits VALUES($1, $2) RETURNING credit", [user.id, 5000]);
+    user.credit = credit[0].credit;
+    res.json({ success, token, user });
+  } else if (userExists === true) {
+    // When posting sign up form if the user exists returns true
+    res.json({ userExists });
+  } else if (anInternalErrorOccurred) {
+    res.json({ anInternalErrorOccurred });
+  }
 };
 
 userAuthControllers.successRedirect = async (req, res) => {
