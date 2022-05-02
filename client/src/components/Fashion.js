56bbc9ea4da93ee7b0ecada5c92a939abbe940a3
programmerXS <cristoferperezmejia80@gmail.com>
2022-05-02 17:28:11 -0400

    Add filter products

--- client/src/components/Fashion.js
+++ client/src/components/Fashion.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Products } from "./ComponentUtils";
 import { getProducts } from "../fetchMethods/get";
-const Fashion = ({ setProductsInCart }) => {
-  const [data, setData] = useState(false);
+const Fashion = ({ setProductsInCart, inputSearchValue }) => {
+  const [products, setProducts] = useState(false);
+  const filterProducts = useRef([]);
 
   useEffect(() => {
-    getProducts("clothes").then((res) => setData(res.clothes));
+    getProducts("clothes").then((res) => {
+      filterProducts.current = res.clothes;
+      setProducts(res.clothes);
+    });
   }, []);
+
+  useEffect(() => {
+    if (inputSearchValue) {
+      setProducts(filterProducts.current.filter((product) => product.title.toLowerCase().includes(inputSearchValue.toLowerCase() || "*")));
+    } else {
+      setProducts(filterProducts.current);
+    }
+  }, [inputSearchValue]);
   return (
     <>
       <section className="w-full pb-20">
         <h1 className="font-bold text-3xl text-center">Man and Woman Fashion</h1>
-        <Products setProductsInCart={setProductsInCart} products={data} />
+        <Products setProductsInCart={setProductsInCart} products={products} inputSearchValue={inputSearchValue} />
       </section>
     </>
   );
