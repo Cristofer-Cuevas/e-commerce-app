dea654097b2353dd73fd1a608e4e8db856aa51b6
programmerXS <cristoferperezmejia80@gmail.com>
2022-05-02 17:29:00 -0400

    Add rendering Routes components from Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,29 +1,44 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useState } from "react";
 import { InputSearch, NavBar } from "./ComponentUtils";
 import cartIcon from "../styles/images/shopping_cart_white_24dp.svg";
 import accountIcon from "../styles/images/account_circle_white_24dp.svg";
 import { UserContext } from "../App";
+import Jewellery from "./Jewellery";
+import Electronic from "./Electronic";
+import Fashion from "./Fashion";
 
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Routes, Route } from "react-router-dom";
 
-const Home = ({ children }) => {
+const Home = ({ children, setProductsInCart }) => {
+  const [inputSearchValue, setInputSearchValue] = useState(null);
+
   return (
     <>
       <section className="bg-hero-image bg-cover bg-center h-5/6">
-        <Header />
+        <Header setInputSearchValue={setInputSearchValue} />
       </section>
       <main className="">
         {children}
         <Outlet />
+        <Routes>
+          <Route path="/" element={[<Fashion key={1} setProductsInCart={setProductsInCart} />, <Jewellery key={2} setProductsInCart={setProductsInCart} />, <Electronic key={3} setProductsInCart={setProductsInCart} />]} />
+          <Route path="/fashion" element={<Fashion inputSearchValue={inputSearchValue} setProductsInCart={setProductsInCart} />} />
+          <Route path="/jewellery" element={<Jewellery setProductsInCart={setProductsInCart} />} />
+          <Route path="/electronic" element={<Electronic setProductsInCart={setProductsInCart} />} />
+        </Routes>
       </main>
     </>
   );
 };
 
 // Header
-const Header = () => {
+const Header = ({ setInputSearchValue }) => {
   const user = useContext(UserContext);
   const inputRef = useRef(null);
+
+  const handleOnInput = (e) => {
+    setInputSearchValue(e.target.value);
+  };
 
   return (
     <header className="w-11/12 mx-auto overflow">
@@ -39,7 +54,7 @@ const Header = () => {
       <div className="flex justify-between items-center mt-4 ">
         <NavBar />
         <div className=" w-4/5 flex rounded-md">
-          <InputSearch inputRef={inputRef} />
+          <InputSearch inputRef={inputRef} onInput={handleOnInput} />
           <span className="bg-search-icon bg-no-repeat bg-center bg-orange-600 inline-block w-10 h-10 rounded-r"> </span>
         </div>
       </div>
