39ba24dee4966d5e5f5842332a5ce567a3f273a8
programmerXS <cristoferperezmejia80@gmail.com>
2022-05-03 21:55:56 -0400

    Add passing var to Products.js to show products on user input

--- client/src/components/Electronic.js
+++ client/src/components/Electronic.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Products } from "./ComponentUtils";
 import { getProducts } from "../fetchMethods/get";
 
-const Electronic = ({ setProductsInCart }) => {
-  const [data, setData] = useState(false);
+const Electronic = ({ setProductsInCart, inputSearchValue }) => {
+  const [products, setProducts] = useState(false);
+  const filterProducts = useRef([]);
 
   useEffect(() => {
-    getProducts("electronics").then((res) => setData(res.electronics));
+    getProducts("electronics").then((res) => {
+      filterProducts.current = res.electronics;
+      setProducts(res.electronics);
+    });
   }, []);
   return (
     <>
       <section className="w-full pb-20">
         <h1 className="font-bold text-3xl text-center">Electronic</h1>
-        <Products setProductsInCart={setProductsInCart} products={data} />
+        <Products setProductsInCart={setProductsInCart} products={products} setProducts={setProducts} filterProducts={filterProducts} inputSearchValue={inputSearchValue} />
       </section>
     </>
   );
