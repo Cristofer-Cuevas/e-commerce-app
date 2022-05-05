64a673408620bee344186a9408179f596b6f6504
programmerXS <cristoferperezmejia80@gmail.com>
2022-05-04 22:15:20 -0400

    Add logic to implement search products

--- client/src/components/Jewellery.js
+++ client/src/components/Jewellery.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Products } from "./ComponentUtils";
 import { getProducts } from "../fetchMethods/get";
 
-const Jewellery = ({ setProductsInCart }) => {
-  const [data, setData] = useState(false);
+const Jewellery = ({ setProductsInCart, inputSearchValue }) => {
+  const [products, setProducts] = useState(false);
+  const filterProducts = useRef(null);
 
   useEffect(() => {
-    getProducts("jewellery").then((res) => setData(res.jewellery));
+    getProducts("jewellery").then((res) => {
+      filterProducts.current = res.jewellery;
+      setProducts(res.jewellery);
+    });
   }, []);
   return (
     <>
       <section className="w-full pb-20">
         <h1 className="font-bold text-3xl text-center">Jewellery</h1>
-        <Products setProductsInCart={setProductsInCart} products={data} />
+        <Products setProductsInCart={setProductsInCart} products={products} filterProducts={filterProducts} inputSearchValue={inputSearchValue} setProducts={setProducts} />
       </section>
     </>
   );
